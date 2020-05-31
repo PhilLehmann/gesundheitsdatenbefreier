0f70f31283e94865c42fcd81621f4ebaf36f8e3a
Phil Lehmann <pl@linux.com>
2020-05-31 12:54:23 +0200

    Version bump, contributors change

--- gesundheitsdatenbefreier.php
+++ gesundheitsdatenbefreier.php
@@ -3,10 +3,10 @@
 == gesundheitsdatenbefreier ==
 Plugin Name: gesundheitsdatenbefreier
 Description: Ein WordPress-Plugin, mit dem Versicherte ihre Daten dank DSGVO befreien können
-Version: 1.5.14
+Version: 1.5.15
 Author: Phil Lehmann, AK Vorratsdatenspeicherung
 Author URI: http://www.vorratsdatenspeicherung.de/
-Contributors: PhilLehmann, BartS23, BSOxid
+Contributors: PhilLehmann, BartS23, BSOxid, mokkin
 Tested up to: 5.2.4
 Requires at least: 5.2.4
 Requires PHP: 7.1
